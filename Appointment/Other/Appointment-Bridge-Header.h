ed20e56e6ee03b9563c38e38444a8f970f54c75c
zhangpeng <zhangpeng@ezbuy.com>
2016-02-05 16:16:24 +0800

    cabbage:首次提交
    增加pod文件

---
+++ Appointment/Other/Appointment-Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Appointment-Bridge-Header.h
+//  Appointment
+//
+//  Created by 张鹏 on 16/1/23.
+//  Copyright © 2016年 SmallCobblerStudio. All rights reserved.
+//
+
+#ifndef Appointment_Bridge_Header_h
+#define Appointment_Bridge_Header_h
+
+#import <AVOSCloud/AVOSCloud.h>
+
+#endif /* Appointment_Bridge_Header_h */
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, "8.0"
+
+pod 'Alamofire'
+
+pod "SVProgressHUD"
+
+pod 'AVOSCloud'
+
+use_frameworks!
